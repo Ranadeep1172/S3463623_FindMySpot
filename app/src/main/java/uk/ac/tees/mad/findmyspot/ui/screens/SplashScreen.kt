e21e235674844fdc6ecd5d8276b5b396729b79f4
Ranadeep1172 <reddyranadeep6@gmail.com>
2025-03-31 12:49:36 +0100

    splash Screen changes

--- app/src/main/java/uk/ac/tees/mad/findmyspot/ui/screens/SplashScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/findmyspot/ui/screens/SplashScreen.kt
@@ -3,6 +3,8 @@ package uk.ac.tees.mad.findmyspot.ui.screens
 
 import android.os.Handler
 import android.os.Looper
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -21,22 +24,37 @@ import uk.ac.tees.mad.findmyspot.R
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    LaunchedEffect(Unit) {
-        delay(2000)
-        val user = FirebaseAuth.getInstance().currentUser
-        if (user != null) {
-            navController.navigate("home") { popUpTo("splash") { inclusive = true } }
-        } else {
-            navController.navigate("auth") { popUpTo("splash") { inclusive = true } }
-        }
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000), label = "fadeAnimation"
+    )
 
+    LaunchedEffect(Unit) {
+        startAnimation = true
+        Handler(Looper.getMainLooper()).postDelayed({
+            val user = FirebaseAuth.getInstance().currentUser
+            startAnimation = false
+        }, 1500)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user != null) {
+                navController.navigate("home") { popUpTo("splash") { inclusive = true } }
+            } else {
+                navController.navigate("auth") { popUpTo("splash") { inclusive = true } }
+            }
+        }, 2000)
     }
 
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.alpha(alphaAnim.value)
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.app_logo),
                 contentDescription = "FindMySpot Logo",
